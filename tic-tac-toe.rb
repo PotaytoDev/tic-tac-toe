6c8943a0ded1df972a63eb96d90770e5f62d055c
Potayto <p.o.tayto@outlook.com>
2022-08-20 14:23:27 -0700

    Add logic to check board's rows to see if a player has won

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,4 +1,6 @@
 class Board
+  attr_reader :board
+
   # The board is a 3x3 two dimensional array created when the object
   # is instantiated.
   def initialize
@@ -43,4 +45,34 @@ class Player
     # Return input as array of [row_index, column_index, player_symbol]
     [row_index, column_index, @player_symbol]
   end
+end
+
+def check_rows(board)
+  winner_declared = true
+
+  board.each_with_index do |row, row_index|
+    winner_declared = true
+
+    row.each_with_index do |_column, column_index|
+      if board[row_index][column_index] == ' '
+        winner_declared = false
+        break
+      end
+
+      next if column_index.zero?
+
+      winner_declared = false if board[row_index][column_index] != board[row_index][column_index - 1]
+    end
+
+    break if winner_declared
+  end
+
+  winner_declared
+end
+
+def check_board_for_winner(board)
+  check_rows(board)
+
+  # check_columns()
+  # check_diagonals()
 end
